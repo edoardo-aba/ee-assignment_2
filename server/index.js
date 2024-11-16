0e09e9e2995389f023f11429b65ab5a9364f5a8a
edoardo-aba <edoardoababei2003@gmail.com>
2024-11-16 14:08:44 +0100

    exported app for the @vercel/node

--- server/index.js
+++ server/index.js
@@ -1,86 +1,70 @@
-require('dotenv').config(); // Import dotenv and configure it
-
-const express = require('express'); // Import Express.js
-const mongoose = require('mongoose'); // Import Mongoose for MongoDB
-const bodyParser = require('body-parser'); // Middleware for parsing request bodies
-const cors = require('cors'); // Middleware for enabling Cross-Origin Resource Sharing
-
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-// Enable CORS for all origins and methods
 app.use(
   cors({
     origin: '*',
-    methods: ['POST', 'GET', 'DELETE', 'PUT'], // Allowed HTTP methods
-    credentials: true, // Allow credentials to be sent
+    methods: ['POST', 'GET', 'DELETE', 'PUT'],
+    credentials: true,
   })
 );
 
-// Middleware to parse JSON
 app.use(bodyParser.json());
 app.use(express.json());
 
-// Connect to MongoDB using the connection string from the .env file
 const mongoUri = process.env.MONGO_URI;
 mongoose.connect(mongoUri);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', async () => {
+db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Middleware to log the request type and request body
 app.use((req, res, next) => {
   console.log(`Request Type: ${req.method}`);
   console.log('Request Body:', req.body);
-  next(); // Proceed to the next middleware or route handler
+  next();
 });
 
-// Define Mongoose Schema for users
-const userSchema = new mongoose.Schema(
-  {
-    name: String,
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    age: Number,
-    gender: String,
-    language: String,
-    educationLevel: String,
-    programmingExperience: Number,
-    occupation: String,
-  },
-  { collection: 'users' }
-);
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  age: Number,
+  gender: String,
+  language: String,
+  educationLevel: String,
+  programmingExperience: Number,
+  occupation: String,
+}, { collection: 'users' });
 
 const User = mongoose.model('User', userSchema);
 
-// Define Mongoose Schema for answers
-const answerSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    email: { type: String, required: true },
-    answers: [
-      {
-        text: { type: String, required: true },
-        selected: { type: String, required: true },
-        correct: { type: Boolean, required: true },
-      },
-    ],
-    timeTaken: { type: Number, required: true }, // Store time taken in seconds
-  },
-  { collection: 'answers' } // Explicitly set the collection name
-);
+const answerSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  email: { type: String, required: true },
+  answers: [
+    {
+      text: { type: String, required: true },
+      selected: { type: String, required: true },
+      correct: { type: Boolean, required: true },
+    },
+  ],
+  timeTaken: { type: Number, required: true },
+}, { collection: 'answers' });
 
 const Answer = mongoose.model('Answer', answerSchema);
 
-// Test route for vercel
 app.get('/', (req, res) => {
   res.json('Hello, World!');
 });
 
-// Endpoint to handle signup
 app.post('/api/signup', async (req, res) => {
   try {
     const { name, email, password, ...otherDetails } = req.body;
@@ -98,13 +82,11 @@ app.post('/api/signup', async (req, res) => {
   }
 });
 
-// Endpoint to handle login
 app.post('/api/login', async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email, password });
-    console.log('User:', user);
     if (!user) {
       return res.status(404).json({ error: 'Invalid email or password' });
     }
@@ -119,16 +101,12 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-// Endpoint to handle submission of answers
 app.post('/api/submit-answers', async (req, res) => {
   try {
     const results = req.body;
 
-    // Save results to the answers collection
     const newAnswer = new Answer(results);
     await newAnswer.save();
-
-    console.log('Received Results:', results);
 
     res.status(200).json({ message: 'Results saved successfully' });
   } catch (error) {
@@ -137,10 +115,5 @@ app.post('/api/submit-answers', async (req, res) => {
   }
 });
 
-// Start server and allow connections from any IP
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-
+// Export the app for @vercel/node
+module.exports = app;
